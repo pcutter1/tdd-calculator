9350ca51106b538a3fae85e1f14aac3e99c28795
Paul C <pcutter@cnm.edu>
2021-04-30 14:09:54 -0600

    Updated to the STOP point

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Calculator {
 
-  static int add(String numbers) {
+  static int add(String numbers) throws IllegalArgumentException {
 
     if (numbers.isEmpty()) {
       return 0;
     }
 
+    if (numbers.startsWith("//")){
+      String delimeter = numbers.substring(2,3);
+      numbers = numbers.replaceAll(delimeter, ",");
+      numbers = numbers.substring(4);
+    }
+
     int[] nums = Stream.of(numbers.split("[,\\n]")).mapToInt(Integer::parseInt).toArray();
+
+    for (int i = 0; i < nums.length; i++) {
+      ArrayList<Integer> negatives = new ArrayList<>();
+      if (nums[i] < 0) {
+        for (int k = i; k < nums.length; k++) {
+          if (nums[k] < 0) {
+            negatives.add(nums[k]);
+          }
+        }
+        throw new IllegalArgumentException("negatives not allowed " + Arrays.toString(negatives.toArray()));
+      }
+    }
 
     if (nums.length == 1){
       return nums[0];
--- test/testCalculator.java
+++ test/testCalculator.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 public class testCalculator {
@@ -56,6 +58,34 @@ public class testCalculator {
     int actual = Calculator.add(multipleNumbers);
 
     assertEquals(expected, actual, "Testing add method for newLines and commas in String");
+  }
+
+  @Test
+  public void testDelimiterSupport() {
+
+    String withDelimiter = "//;\n1;2";
+    int expected = 3;
+    int actual = Calculator.add(withDelimiter);
+
+    assertEquals(expected, actual, "Testing delimiter support for add method");
+  }
+
+  @Test
+  public void testNegativeException() {
+    String withNegatives = "1,2,4,-4,10,-82";
+    assertThrows(IllegalArgumentException.class, () -> {Calculator.add(withNegatives);});
+  }
+
+  @Test
+  public void testNegativeExceptionMessage() {
+
+    String withNegatives = "1,2,4,-4,10,-82";
+    String expected = "negatives not allowed [-4, -82]";
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {Calculator.add(withNegatives);});
+
+    String actual = exception.getMessage();
+
+    assertEquals(expected, actual, "Testing the exception message for negative numbers");
   }
 
 }
